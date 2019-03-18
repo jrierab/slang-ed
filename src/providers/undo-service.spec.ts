79590da2d4fc51a1a3182af2d8cb884c98628537
Jordi Riera-Babures <jrierab@gmail.com>
2019-03-18 23:58:25 +0100

    feat: Unit testing added to debug undo service

--- src/providers/undo-service.spec.ts
+++ src/providers/undo-service.spec.ts
@@ -1,5 +1,5 @@
 import { UndoService } from "./undo-service";
-import { langTranslationsObject } from "../customTypes/langObject.types";
+import { langTranslationsObject, langNodeObject } from "../customTypes/langObject.types";
 
 // 
 // npm link @angular/cli
@@ -8,19 +8,11 @@ import { langTranslationsObject } from "../customTypes/langObject.types";
 
 // Straight Jasmine testing without Angular's testing support
 describe('UndoService', () => {
-    let undoService: UndoService;
+    const undoService: UndoService = new UndoService();
     let translations: langTranslationsObject;
 
     beforeEach(() => { 
-        undoService = new UndoService();
-        const k1 : langNodeObject = {
-
-        };
-        const k2 : langNodeObject = {
-
-        };
-
-        translations = {
+        const t: langTranslationsObject = {    
             options : null,
             languages : [],
             root: {
@@ -28,17 +20,90 @@ describe('UndoService', () => {
                 full_key: "",
                 isLeaf: false,
                 level: 0,
-                nodes: [k1, k2]
+                nodes: []
             }
+        };
+        for(let n_l1=0; n_l1<2; n_l1++) {
+            const key_l1: string = "key_"+n_l1.toString();
+            const node_l1: langNodeObject = {
+                key: key_l1,
+                full_key: key_l1,
+                isLeaf : false,
+                level: 1,
+                nodes: []
+            }
+            for(let n_l2=0; n_l2<4; n_l2++) {
+                const key_l2: string = "key_"+n_l2.toString();
+                const node_l2: langNodeObject = {
+                    key: key_l2,
+                    full_key: key_l1+"."+key_l2,
+                    isLeaf : true,
+                    level: 2,
+                    nodes: []
+                }
+                for(let v_l2=0; v_l2<2; v_l2++) {
+                    const lang: string = (v_l2===0? "ca": "en");
+                    node_l2.nodes.push({
+                        lang: lang,
+                        value: "value_"+lang+"_"+n_l1.toString()+"_"+n_l2.toString(),
+                        comment: "",
+                        approved: false,
+                        foundInSrc: false,
+                        preserve: false
+                    });
+                }
+                node_l1.nodes.push(node_l2);
+            }
+            t.root.nodes.push(node_l1);
         }
+        translations = JSON.parse(JSON.stringify(t));
     });
     
-    it('#getValue should return real value', () => {
-        this.undoService.clearHistory(this.translations);
+    it('ClearHistory test', () => {
+        const myTranslations = JSON.parse(JSON.stringify(translations));
 
-        expect(undoService.getValue()).toBe('real value');
+        // Init and start
+        undoService.clearHistory(myTranslations);
+
+        expect(undoService.hasHistory()).toBe(false, "hasHistory");
+        expect(undoService.hasFuture()).toBe(false, "hasFuture");
     });
-    
+
+        
+    it('Undo/Redo 1 cycle', () => {
+        let myTranslations = JSON.parse(JSON.stringify(translations));
+
+        //console.log(JSON.stringify(myTranslations));
+
+        // Init and start
+        undoService.clearHistory(myTranslations);
+
+        // Click one key, but do not modify it
+        undoService.rememberThisHistory("key_0", myTranslations);
+        
+        expect(undoService.hasHistory()).toBe(false, "hasHistory");
+        expect(undoService.hasFuture()).toBe(false, "hasFuture");
+        
+        // Click another key and modify it
+        undoService.rememberThisHistory("key_1", myTranslations);
+        myTranslations.root.nodes[1].key = "key_1_mod";
+
+        // Click another key - this will store the new data
+        undoService.rememberThisHistory("key_2", myTranslations);
+        
+        expect(undoService.hasHistory()).toBe(true, "hasHistory");
+        expect(undoService.hasFuture()).toBe(false, "hasFuture");
+
+        // UNDO
+        myTranslations = undoService.undo(myTranslations);
+
+        // Should have recovered
+        expect(myTranslations.root.nodes[1].key).toBe("key_1", "UNDO key_1");
+
+        expect(undoService.hasHistory()).toBe(false, "hasHistory");
+        expect(undoService.hasFuture()).toBe(true, "hasFuture");
+    });
+
     /*
     it('#getObservableValue should return value from observable',
     (done: DoneFn) => {
