07bff4d48d0d45cd0e51f6def36642d28e7f41a1
Jordi Riera-Babures <jrierab@gmail.com>
2019-03-28 23:24:23 +0100

    feat: Adds translations + info about dir + styles

--- src/components/list-word/list-word.ts
+++ src/components/list-word/list-word.ts
@@ -9,41 +9,41 @@ import { LangNodeObject, LangTopicObject } from '../../customTypes/langObject.ty
  * Components.
  */
 @Component({
-  selector: 'list-word',
-  templateUrl: 'list-word.html',
-  styleUrls: ['list-word.scss']
+    selector: 'list-word',
+    templateUrl: 'list-word.html',
+    styleUrls: ['list-word.scss']
 })
 export class ListWordComponent implements OnChanges {
-  @Input() word: LangNodeObject;
-  @Input() list: LangNodeObject;
+    @Input() word: LangNodeObject;
+    @Input() list: LangNodeObject;
 
-  words: Array<LangNodeObject | LangTopicObject>;
+    words: Array<LangNodeObject | LangTopicObject>;
 
-  showChilds = false;
+    showChilds = false;
 
-  indents: Array<number>;
+    indents: Array<number>;
 
-  constructor(private langTools: LangToolsService) {
-    console.log('### ListWordComponent');
-  }
-
-  ngOnChanges() {
-    this.indents = Array(this.word.level).fill(0).map((x, i) => i);
-
-    if (this.list.isLeaf) {
-      this.words = [];
-    } else {
-      this.words = this.word.nodes;
-      // this.langTools.sort(this.words as Array<langNodeObject>);
+    constructor(private langTools: LangToolsService) {
+        console.log('### ListWordComponent');
     }
-  }
 
-  public doClick() {
-    if (!this.word.isLeaf) {
-      this.showChilds = !this.showChilds;
+    ngOnChanges() {
+        this.indents = Array(this.word.level - 1).fill(0).map((x, i) => i);
+
+        if (this.list.isLeaf) {
+            this.words = [];
+        } else {
+            this.words = this.word.nodes;
+            // this.langTools.sort(this.words as Array<langNodeObject>);
+        }
     }
-    this.langTools.doRememberTranslations(this.word.full_key);
-    this.langTools.doEditWord(this.word);
-  }
+
+    public doClick() {
+        if (!this.word.isLeaf) {
+            this.showChilds = !this.showChilds;
+        }
+        this.langTools.doRememberTranslations(this.word.full_key);
+        this.langTools.doEditWord(this.word);
+    }
 
 }
--- src/pages/home/home.page.ts
+++ src/pages/home/home.page.ts
@@ -8,192 +8,204 @@ import { LangFileObject, LangTranslationsObject, LangNodeObject, LangTopicObject
 import { UndoService } from '../../providers/undo-service';
 
 @Component({
-  selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss']
+    selector: 'app-home',
+    templateUrl: 'home.page.html',
+    styleUrls: ['home.page.scss']
 })
 export class HomePage implements OnInit {
-  private translations: LangTranslationsObject = this.langTools.clearTranslations();
+    private translations: LangTranslationsObject = this.langTools.clearTranslations();
 
-  level: LangNodeObject;
-  words: Array<LangNodeObject | LangTopicObject>;
+    level: LangNodeObject;
+    words: Array<LangNodeObject | LangTopicObject>;
 
-  projectNeedsSaving = false;
-  projectReady = false;
-  addNodeOrIdReady = false;
-  removeNodeReady = false;
+    projectNeedsSaving = false;
+    projectReady = false;
+    addNodeOrIdReady = false;
+    removeNodeReady = false;
 
-  constructor(private electron: ElectronProvider,
-    private langTools: LangToolsService,
-    private undoService: UndoService,
-    private alertCtrl: AlertController
-  ) {
-  }
+    constructor(private electron: ElectronProvider,
+        private langTools: LangToolsService,
+        private undoService: UndoService,
+        private alertCtrl: AlertController
+    ) {
+    }
 
-  ngOnInit() {
-    this.langTools.getCurrentlyEditedWord().subscribe((data: LangNodeObject) => {
-      this.addNodeOrIdReady = false;
-      this.removeNodeReady = false;
-      if (data) {
-        this.removeNodeReady = true;
-        if (!data.isLeaf) {
-          this.addNodeOrIdReady = true;
+    ngOnInit() {
+        this.langTools.getCurrentlyEditedWord().subscribe((data: LangNodeObject) => {
+            this.addNodeOrIdReady = false;
+            this.removeNodeReady = false;
+            if (data) {
+                this.removeNodeReady = true;
+                if (!data.isLeaf) {
+                    this.addNodeOrIdReady = true;
+                }
+            }
+        });
+    }
+
+    doNewProject() {
+        console.log('Create New project');
+    }
+
+    doInitFrom() {
+        const folder = this.electron.selectFolder();
+
+        // console.log("> Selected folder: "+folder);
+
+        if (folder) {
+            const path_to_i18n = this.electron.findTranslationsFolder(folder);
+
+            // console.log("> i18n folder: "+path_to_i18n);
+
+            if (path_to_i18n) {
+                const langFiles: Array<LangFileObject> = this.electron.readTranslationsFiles(path_to_i18n);
+
+                this.translations = this.langTools.initTranslations(langFiles);
+
+                // Remember paths
+                this.translations.options.projectFolder = folder;
+                this.translations.options.i18nFolder = path_to_i18n;
+
+                this.doInitFromTranslations(true);
+            }
         }
-      }
-    });
-  }
-
-  doNewProject() {
-    console.log('Create New project');
-  }
-
-  doInitFrom() {
-    const folder = this.electron.selectFolder();
-
-    // console.log("> Selected folder: "+folder);
-
-    if (folder) {
-      const path_to_i18n = this.electron.findTranslationsFolder(folder);
-
-      // console.log("> i18n folder: "+path_to_i18n);
-
-      if (path_to_i18n) {
-        const langFiles: Array<LangFileObject> = this.electron.readTranslationsFiles(path_to_i18n);
-
-        this.translations = this.langTools.initTranslations(langFiles);
-
-        // Remember paths
-        this.translations.options.projectFolder = folder;
-        this.translations.options.i18nFolder = path_to_i18n;
-
-        this.doInitFromTranslations(true);
-      }
     }
-  }
 
-  doInitFromTranslations(shouldInit: boolean = false) {
-    this.level = this.translations.root;
-    this.words = this.level.nodes;
-    // this.langTools.sort(this.words as Array<langNodeObject>);
-    this.doSortTranslations(this.level);
+    doInitFromTranslations(shouldInit: boolean = false) {
+        this.level = this.translations.root;
+        this.words = this.level.nodes;
+        // this.langTools.sort(this.words as Array<langNodeObject>);
+        this.doSortTranslations(this.level);
 
-    this.projectNeedsSaving = true;
-    this.projectReady = true;
+        this.projectNeedsSaving = true;
+        this.projectReady = true;
 
-    if (shouldInit) {
-      this.undoService.clearHistory(this.translations);
+        if (shouldInit) {
+            this.undoService.clearHistory(this.translations);
+        }
     }
-  }
 
-  doSortTranslations(node: LangNodeObject) {
-    this.langTools.sort(node.nodes as Array<LangNodeObject>);
-    node.nodes.forEach((n: LangNodeObject) => {
-      if (!n.isLeaf) {
-        this.doSortTranslations(n);
-      }
-    });
-  }
-
-  doOpenProject() {
-    console.log('Open existing project');
-  }
-
-  doSaveProject() {
-    if (this.projectNeedsSaving) {
-      console.log('Save current project');
+    doSortTranslations(node: LangNodeObject) {
+        this.langTools.sort(node.nodes as Array<LangNodeObject>);
+        node.nodes.forEach((n: LangNodeObject) => {
+            if (!n.isLeaf) {
+                this.doSortTranslations(n);
+            }
+        });
     }
-  }
 
-  doSaveTranslations() {
-    if (this.langTools.isTranslationsSavingRequired()) {
-      if (this.electron.writeTranslationFiles(this.translations, false)) {
-        this.langTools.doTranslationNeedsSaving(false);
-      } else {
-        this.langTools.doTranslationNeedsSaving(true);
-      }
+    doOpenProject() {
+        console.log('Open existing project');
     }
-  }
 
-  doOpenSettings() {
-    console.log('Open project settings');
-  }
+    doSaveProject() {
+        if (this.projectNeedsSaving) {
+            console.log('Save current project');
+        }
+    }
 
-  doAddRootNode() {
-    this.langTools.doClearEditWord();
+    doSaveTranslations() {
+        if (this.langTools.isTranslationsSavingRequired()) {
+            if (this.electron.writeTranslationFiles(this.translations, false)) {
+                this.langTools.doTranslationNeedsSaving(false);
+            } else {
+                this.langTools.doTranslationNeedsSaving(true);
+            }
+        }
+    }
 
-    const newNode: LangNodeObject = this.langTools.createNodeAtLevel(this.level, 'New_Node');
-    this.projectNeedsSaving = true;
-    this.langTools.doTranslationNeedsSaving(true);
-    this.langTools.doEditWord(newNode);
-  }
+    doOpenSettings() {
+        console.log('Open project settings');
+    }
 
-  doAddNode() {
-    const level: LangNodeObject = this.langTools.getCurrentlyEditedWordValue();
+    doShowAbout() {
+        console.log('Show about');
+    }
 
-    this.langTools.doClearEditWord();
-    const newNode: LangNodeObject = this.langTools.createNodeAtLevel(level, 'New_Node');
-    this.projectNeedsSaving = true;
-    this.langTools.doTranslationNeedsSaving(true);
-    this.langTools.doEditWord(newNode);
-  }
-
-  doAddId() {
-    const level: LangNodeObject = this.langTools.getCurrentlyEditedWordValue();
-
-    this.langTools.doClearEditWord();
-    const newId: LangNodeObject = this.langTools.createIdAtLevel(level, 'New_Id');
-    this.projectNeedsSaving = true;
-    this.langTools.doTranslationNeedsSaving(true);
-    this.langTools.doEditWord(newId);
-  }
-
-  async doRemoveCurrent() {
-    const level: LangNodeObject = this.langTools.getCurrentlyEditedWordValue();
-    const nodes = this.langTools.countDescendants(level);
-
-    const confirm = await this.alertCtrl.create({
-      header: 'Remove "' + level.full_key + '" ?',
-      message: 'This will DELETE ' + nodes + ' language definitions !',
-      buttons: [
-        {
-          text: 'Cancel'
-        },
-        {
-          text: 'Agree',
-          handler: () => {
+    doAddRootNode() {
+        if (this.projectReady) {
             this.langTools.doClearEditWord();
-            this.langTools.removeNode(level);
+
+            const newNode: LangNodeObject = this.langTools.createNodeAtLevel(this.level, 'New_Node');
             this.projectNeedsSaving = true;
             this.langTools.doTranslationNeedsSaving(true);
-          }
+            this.langTools.doEditWord(newNode);
         }
-      ]
-    });
-    confirm.present();
-  }
-
-  doUndo() {
-    if (this.undoService.hasHistory()) {
-      this.translations = this.langTools.undo();
-      this.doInitFromTranslations();
-      // console.log(this.level);
     }
-  }
 
-  doRedo() {
-    if (this.undoService.hasFuture()) {
-      this.translations = this.langTools.redo();
-      this.doInitFromTranslations();
-      // console.log(this.level);
+    doAddNode() {
+        if (this.addNodeOrIdReady) {
+            const level: LangNodeObject = this.langTools.getCurrentlyEditedWordValue();
+
+            this.langTools.doClearEditWord();
+            const newNode: LangNodeObject = this.langTools.createNodeAtLevel(level, 'New_Node');
+            this.projectNeedsSaving = true;
+            this.langTools.doTranslationNeedsSaving(true);
+            this.langTools.doEditWord(newNode);
+        }
     }
-  }
 
-  doZoomIn() {
-    this.electron.zoomIn();
-  }
+    doAddId() {
+        if (this.addNodeOrIdReady) {
+            const level: LangNodeObject = this.langTools.getCurrentlyEditedWordValue();
 
-  doZoomOut() {
-    this.electron.zoomOut();
-  }
+            this.langTools.doClearEditWord();
+            const newId: LangNodeObject = this.langTools.createIdAtLevel(level, 'New_Id');
+            this.projectNeedsSaving = true;
+            this.langTools.doTranslationNeedsSaving(true);
+            this.langTools.doEditWord(newId);
+        }
+    }
+
+    async doRemoveCurrent() {
+        if (this.removeNodeReady) {
+            const level: LangNodeObject = this.langTools.getCurrentlyEditedWordValue();
+            const nodes = this.langTools.countDescendants(level);
+
+            const confirm = await this.alertCtrl.create({
+                header: 'Remove "' + level.full_key + '" ?',
+                message: 'This will DELETE ' + nodes + ' language definitions !',
+                buttons: [
+                    {
+                        text: 'Cancel'
+                    },
+                    {
+                        text: 'Agree',
+                        handler: () => {
+                            this.langTools.doClearEditWord();
+                            this.langTools.removeNode(level);
+                            this.projectNeedsSaving = true;
+                            this.langTools.doTranslationNeedsSaving(true);
+                        }
+                    }
+                ]
+            });
+            confirm.present();
+        }
+    }
+
+    doUndo() {
+        if (this.undoService.hasHistory()) {
+            this.translations = this.langTools.undo();
+            this.doInitFromTranslations();
+            // console.log(this.level);
+        }
+    }
+
+    doRedo() {
+        if (this.undoService.hasFuture()) {
+            this.translations = this.langTools.redo();
+            this.doInitFromTranslations();
+            // console.log(this.level);
+        }
+    }
+
+    doZoomIn() {
+        this.electron.zoomIn();
+    }
+
+    doZoomOut() {
+        this.electron.zoomOut();
+    }
 
 }
